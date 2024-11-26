f8ce2272f65003ce704adea10d43215888b5a107
noahrognon <noah.rognon@edu.univ-fcomte.fr>
2024-11-26 16:49:27 +0100

    referme les détails suite à un clic sur un summary

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
-// *** ACCORDEON ***
+
+const accordeon = document.querySelector(".accordeon");
+
+
+accordeon.querySelectorAll("summary").forEach((summary) => {
+    summary.addEventListener("click", (event) => {
+        const details = event.target.closest("details");
+        accordeon.querySelectorAll("details").forEach((det) => {
+            if (det !== details) {
+                det.open = false;
+            }
+        });
+    });
+});
